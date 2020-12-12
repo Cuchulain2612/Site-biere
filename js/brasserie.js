146ca817f33e1c62e3d7432938d4a1b7dc9768eb
Cuchulain2612 <71312603+Cuchulain2612@users.noreply.github.com>
2020-12-12 20:18:12 +0100

    test envoi

--- js/brasserie.js
+++ js/brasserie.js
@@ -2,6 +2,13 @@
 
 const ageAlcool=16;   // à transformer en millisecondes pour le if
 
+var panierCourse={
+    beerUn:{nombre:0},
+    beerDeux:{nombre:0},
+    beerTrois:{nombre:0},
+    beerQuatre:{nombre:0},
+}
+
 function verifAge() {
     let naissance=document.getElementById("dateNaissance").value;    // on récupères un ISOString qu'on transforme en dte à la prochaine ligne
     let age= new Date()- new Date(naissance);   // résultat en millisecondes
@@ -25,4 +32,28 @@ function verifAge() {
  */
 function redirection(adresse) {
     window.location.href=adresse;      // différence entre document et window    -> synonymes      NE FONCTIONNE PAS -> ????
+    return false;
+}
+
+function ajouterPanier(beer){
+    beer.nombre+=1;
+    return false;
+}
+
+function retirerPanier(beer){
+    if (beer.nombre>0){
+        beer.nombre-=1;
+    }
+    else{
+        alert("Il n'en reste plus dans votre panier.")
+    }
+    return false;
+}
+
+function afficherPanierTest() {
+    var clefs= Object.keys(panierCourse);
+    for(let i of clefs){
+        console.log(panierCourse[i], panierCourse[i].nombre);          //QUESTION : Pourquoi devoir utiliser [] puis .
+    }
+    return false;
 }
